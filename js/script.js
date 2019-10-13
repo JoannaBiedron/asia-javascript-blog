61c988efe2b7659b5ced12d8c4df121392c4bd4b
Joanna Biedroń <joanna.biedron@gmail.com>
2019-10-13 18:10:14 +0200

    Add js code -remove class 'activr' from all articles

--- js/script.js
+++ js/script.js
@@ -15,9 +15,15 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('article');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
