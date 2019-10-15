4cb22bd475bdefce266016e11432f6002883b2e0
Joanna Biedroń <joanna.biedron@gmail.com>
2019-10-15 21:28:18 +0200

    Add js code - find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -31,7 +31,8 @@ clickedElement.classList.add('active');
 const articleSelector = clickedElement.getAttribute('href');
 console.log('articleSelector: ', articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+const targetArticle = document.querySelector(articleSelector);
+console.log('targetArticle: ', targetArticle);
   /* add class 'active' to the correct article */
 
 }
