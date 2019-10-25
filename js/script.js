13755b47e34e0012c2b43d4ed8fc609498b8b301
Joanna Biedroń <joanna.biedron@gmail.com>
2019-10-25 15:34:42 +0200

    Add js code -tagClickHandler

--- js/script.js
+++ js/script.js
@@ -141,13 +141,15 @@ generateTags();
 
 function tagClickHandler(event){
   /* prevent default action for this event */
-
+  event.preventDefault();
   /* make new constant named "clickedElement" and give it the value of "this" */
-
+  const clickedElement = this;
+  console.log('clickedElement:  ',clickedElement);
   /* make a new constant "href" and read the attribute "href" of the clicked element */
-
+  const href = clickedElement.getAttribute('href')
+  console.log('href: ', href);
   /* make a new constant "tag" and extract tag from the "href" constant */
-
+  const tag = href.replace('#tag-', '');
   /* find all tag links with class active */
 
   /* START LOOP: for each active tag link */
